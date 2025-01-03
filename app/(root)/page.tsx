e077abf6769280c1ed85fc245eadb997ab0ef3fe
Vikash Anand <vaui0906@gmail.com>
2025-01-03 09:56:31 +0530

    Changes: Implemented Github authentication in the application
    ---
    1.Implemented Github OAuth authentication
    2.Created folder structure for authentication process within Next.js

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <>
+      <h1 className='text-2xl'>Home</h1>
+    </>
+  )
+}
+
+export default page
