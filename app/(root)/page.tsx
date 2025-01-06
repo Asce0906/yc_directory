d0433604b02b544c0102c63d97e101364359cebf
Vikash Anand <vaui0906@gmail.com>
2025-01-06 11:02:53 +0530

    Changes: Created a startup card components to render various startup-cards in the app

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,38 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+
+type StartupCardType = {
+  _id: number,
+  _createdAt: string,
+  views: number,
+  author: {
+    _id: number,
+    name: string
+  },
+  description: string,
+  image: string,
+  category: string,
+  title: string
+}
 
 const page = async ({ searchParams }: { searchParams: Promise<{ query?: string }> }) => {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _id: 1,
+      _createdAt: new Date().toISOString(),
+      views: 55,
+      author: {
+        _id: 1,
+        name: 'Audrian'
+      },
+      description: 'This is a description',
+      image: 'https://images.pexels.com/photos/2085832/pexels-photo-2085832.jpeg?auto=compress&cs=tinysrgb&w=600',
+      category: 'Robots',
+      title: 'We Robots'
+    },
+  ];
 
   return (
     <>
@@ -14,6 +45,23 @@ const page = async ({ searchParams }: { searchParams: Promise<{ query?: string }
           Submit Ideas, Vote on pitches, and get Noticed in virtual competition
         </p>
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : 'All StartUps'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post._id} post={post}/>
+            ))
+          ): (
+            <p className="no-results">No startups found</p>
+          )}
+            
+        </ul>
       </section>
     </>
   )
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns : [
+      {
+        protocol: 'https',
+        // hostname: 'images.pexels.com'     //allow images from a particular source
+        hostname: '*'     //allow images from all the sources
+      }
+    ]
+  }
 };
 
 export default nextConfig;
